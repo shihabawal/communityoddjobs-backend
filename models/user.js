f4bc39cb610a0d3a4c3feec8efc03bcec8b90688
hemms87 <hemanth.infosys@gmail.com>
2022-03-31 16:23:56 +0100

    Backend change for creating user

--- models/user.js
+++ models/user.js
@@ -2,17 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var AddressSchema = new Schema({
+    zip: { type: String, required: true },
     country: { type: String, required: true },
+    state: { type: String, required: true },
     city: { type: String, required: true },
     line1: { type: String, required: true },
-    line2: { type: String }
 });
 
 var UserSchema = new Schema({
     name: { type: String, required: true },
     address: { type: AddressSchema, required: true },
     contact: { type: Number, required: true },
-    location: { type: String },
     email: { type: String, required: true },
     password: { type: String, required: true },
     admin: { type: Boolean, required: true },
