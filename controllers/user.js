6eb5ba89ff5da7e3ea057b4f876c1e4a7c544975
mariaayoub58 <mariaayoub.se@gmail.com>
2022-04-14 18:08:50 +0100

    Created User Listing screen

--- controllers/user.js
+++ controllers/user.js
@@ -1,161 +1,220 @@
-var User = require('../models/user').User;
-var utils = require('./utils');
+var User = require("../models/user").User;
+var utils = require("./utils");
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
-    res.send({ status: 'success', message: 'Greetings from the User controller!' });
+  res.send({
+    status: "success",
+    message: "Greetings from the User controller!",
+  });
 };
 
 exports.get_all_users = function (req, res) {
-    utils.checkAdmin(req, res, (req, res) => {
-        User.find({}, (err, doc) => {
-            if (err) {
-                res.send({ status: 'error', message: `An error occured. ${err.message && err.message}` });
-                return;
-            } else {
-                // if email found ... 
-                if (doc) {
-                    // ... check password
-                    if (doc.password === req.body.password) {
-                        // if correct return user details
-                        res.send({ status: 'success', message: 'Found users', data: doc });
-                    } else {
-                        res.send({ status: 'error', message: 'Incorrect email or password' });
-                    }
-                } else {
-                    res.send({ status: 'error', message: 'Incorrect email or password' });
-                }
-            }
-        })
-
+  utils.checkAdmin(req, res, (req, res) => {
+    User.find({}, (err, doc) => {
+      if (err) {
+        res.send({
+          status: "error",
+          message: `An error occured. ${err.message && err.message}`,
+        });
+        return;
+      } else {
+        // if email found ...
+        if (doc) {
+          // ... check password
+          if (doc.password === req.body.password) {
+            // if correct return user details
+            res.send({ status: "success", message: "Found users", data: doc });
+          } else {
+            res.send({
+              status: "error",
+              message: "Incorrect email or password",
+            });
+          }
+        } else {
+          res.send({ status: "error", message: "Incorrect email or password" });
+        }
+      }
     });
-}
+  });
+};
+
+exports.user_search = function (req, res) {
+  JobListing.find(
+    {
+      name: { $regex: `.*${req.body.searchString}.*`, $options: "i" },
+    },
+    function (err, doc) {
+      if (err) {
+        res.send({
+          status: "error",
+          message: `An error occured. ${err.message && err.message}`,
+        });
+        return;
+      } else {
+        if (doc)
+          res.send({ status: "success", message: "User found", data: doc });
+        else res.send({ status: "success", message: "Not found" });
+      }
+    }
+  );
+};
 
 exports.user_create = async function (req, res) {
-    // check if user exists and wait
-    var email = await User.findOne({ email: req.body.email }).exec();
-    if (email) {
-        res.send({ status: 'error', message: 'Email already exists' });
-        return;
+  // check if user exists and wait
+  var email = await User.findOne({ email: req.body.email }).exec();
+  if (email) {
+    res.send({ status: "error", message: "Email already exists" });
+    return;
+  }
+
+  var user = new User({
+    name: req.body.name,
+    address: req.body.address,
+    contact: req.body.contact,
+    email: req.body.email,
+    password: req.body.password,
+    admin: false,
+    created: new Date(),
+    notifications: [],
+  });
+
+  user.save(function (err, doc) {
+    if (err) {
+      res.send({
+        status: "error",
+        message: `An error occured. ${err.message && err.message}`,
+      });
+      return;
+    } else {
+      res.send({ status: "success", message: "User created", data: doc });
     }
-
-    var user = new User({
-        name: req.body.name,
-        address: req.body.address,
-        contact: req.body.contact,
-        email: req.body.email,
-        password: req.body.password,
-        admin: false,
-        created: new Date(),
-        notifications: []
-    });
-
-    user.save(function (err, doc) {
-        if (err) {
-            res.send({ status: 'error', message: `An error occured. ${err.message && err.message}` })
-            return;
-        } else {
-            res.send({ status: 'success', message: 'User created', data: doc });
-        }
-    });
-}
+  });
+};
 
 exports.admin_create = async function (req, res) {
-    var email = await User.findOne({ email: req.body.email }).exec();
+  var email = await User.findOne({ email: req.body.email }).exec();
 
-    if (email) {
-        res.send({ status: 'error', message: 'Admin email already exists' });
-        return;
+  if (email) {
+    res.send({ status: "error", message: "Admin email already exists" });
+    return;
+  }
+
+  var user = new User({
+    name: req.body.name,
+    address: req.body.address,
+    contact: req.body.contact,
+    email: req.body.email,
+    password: req.body.password,
+    admin: true,
+    created: new Date(),
+  });
+
+  user.save(function (err) {
+    if (err) {
+      res.send({
+        status: "error",
+        message: `An error occured. ${err.message && err.message}`,
+      });
+      return;
+    } else {
+      res.send({ status: "success", message: "Admin created successfully" });
     }
-
-    var user = new User({
-        name: req.body.name,
-        address: req.body.address,
-        contact: req.body.contact,
-        email: req.body.email,
-        password: req.body.password,
-        admin: true,
-        created: new Date()
-    });
-
-    user.save(function (err) {
-        if (err) {
-            res.send({ status: 'error', message: `An error occured. ${err.message && err.message}` })
-            return;
-        } else {
-            res.send({ status: 'success', message: 'Admin created successfully' });
-        }
-    });
-}
+  });
+};
 
 exports.login = function (req, res) {
-    User.findOne({ email: req.body.email }, (err, doc) => {
-        if (err) {
-            res.send({ status: 'error', message: `An error occured. ${err.message && err.message}` });
-            return;
+  User.findOne({ email: req.body.email }, (err, doc) => {
+    if (err) {
+      res.send({
+        status: "error",
+        message: `An error occured. ${err.message && err.message}`,
+      });
+      return;
+    } else {
+      // if email found ...
+      if (doc) {
+        // ... check password
+        if (doc.password === req.body.password) {
+          // if correct return user details
+          res.send({ status: "success", message: "Logged in", data: doc });
         } else {
-            // if email found ... 
-            if (doc) {
-                // ... check password
-                if (doc.password === req.body.password) {
-                    // if correct return user details
-                    res.send({ status: 'success', message: 'Logged in', data: doc });
-                } else {
-                    res.send({ status: 'error', message: 'Incorrect email or password' });
-                }
-            } else {
-                res.send({ status: 'error', message: 'Incorrect email or password' });
-            }
+          res.send({ status: "error", message: "Incorrect email or password" });
         }
-    })
-}
+      } else {
+        res.send({ status: "error", message: "Incorrect email or password" });
+      }
+    }
+  });
+};
 
 exports.user_edit = function (req, res) {
-    utils.checkAdmin(req, res, (req, res) => {
-        User.findOneAndUpdate({ email: req.body.user.email }, { ...req.body.user }, (err) => {
-            if (err) {
-                res.send({ status: 'error', message: `An error occured. ${err.message && err.message}` });
-            } else {
-                res.send({ status: 'success', message: 'Updated successfully' })
-            }
-        });
-    });
-}
+  utils.checkAdmin(req, res, (req, res) => {
+    User.findOneAndUpdate(
+      { email: req.body.user.email },
+      { ...req.body.user },
+      (err) => {
+        if (err) {
+          res.send({
+            status: "error",
+            message: `An error occured. ${err.message && err.message}`,
+          });
+        } else {
+          res.send({ status: "success", message: "Updated successfully" });
+        }
+      }
+    );
+  });
+};
 
 exports.user_email_delete = function (req, res) {
-    utils.checkAdmin(req, res, (req, res) => {
-        User.findOneAndRemove({ email: req.body.email }, function (err, doc) {
-            if (err) {
-                res.send({ status: 'error', message: `An error occured. ${err.message && err.message}` })
-                return;
-            } else {
-                res.send({ status: 'success', message: 'Deleted successfully' });
-            }
-        })
+  utils.checkAdmin(req, res, (req, res) => {
+    User.findOneAndRemove({ email: req.body.email }, function (err, doc) {
+      if (err) {
+        res.send({
+          status: "error",
+          message: `An error occured. ${err.message && err.message}`,
+        });
+        return;
+      } else {
+        res.send({ status: "success", message: "Deleted successfully" });
+      }
     });
+  });
 };
 
 exports.user_delete = function (req, res) {
-    utils.checkAdmin(req, res, (req, res) => {
-        User.findByIdAndRemove(req.params.id, function (err) {
-            if (err) {
-                res.send({ status: 'error', message: `An error occured. ${err.message && err.message}` })
-                return;
-            } else {
-                res.send({ status: 'success', message: 'Deleted successfully' });
-            }
-        })
+  utils.checkAdmin(req, res, (req, res) => {
+    User.findByIdAndRemove(req.params.id, function (err) {
+      if (err) {
+        res.send({
+          status: "error",
+          message: `An error occured. ${err.message && err.message}`,
+        });
+        return;
+      } else {
+        res.send({ status: "success", message: "Deleted successfully" });
+      }
     });
+  });
 };
 
 exports.user_clear_notifications = function (req, res) {
-    User.findOneAndUpdate({ email: req.body.email },
-        { notifications: [] }
-        , function (err) {
-            if (err) {
-                res.send({ status: 'error', message: `An error occured. ${err.message && err.message}` })
-            } else {
-                res.send({ status: 'success', message: 'Cleared all notifications. Login again' })
-            }
-        })
+  User.findOneAndUpdate(
+    { email: req.body.email },
+    { notifications: [] },
+    function (err) {
+      if (err) {
+        res.send({
+          status: "error",
+          message: `An error occured. ${err.message && err.message}`,
+        });
+      } else {
+        res.send({
+          status: "success",
+          message: "Cleared all notifications. Login again",
+        });
+      }
+    }
+  );
 };
--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var user_controller = require('../controllers/user');
+var user_controller = require("../controllers/user");
 
-// test 
-router.get('/test', user_controller.test);
+// test
+router.get("/test", user_controller.test);
+
+//search user by name
+router.post("/search", user_controller.user_search);
 
 // Get all users
-router.post('/get', user_controller.get_all_users);
+router.post("/get", user_controller.get_all_users);
 
 // create new user
-router.post('/create', user_controller.user_create);
+router.post("/create", user_controller.user_create);
 
 // create new admin
-router.post('/admincreate', user_controller.admin_create);
+router.post("/admincreate", user_controller.admin_create);
 
 // admin and user login
-router.post('/login', user_controller.login);
+router.post("/login", user_controller.login);
 
 // admin and user login
-router.put('/edit', user_controller.user_edit);
+router.put("/edit", user_controller.user_edit);
 
 // delete by email
-router.delete('/delete', user_controller.user_email_delete);
+router.delete("/delete", user_controller.user_email_delete);
 
 // delete by id
-router.delete('/:id/delete', user_controller.user_delete);
+router.delete("/:id/delete", user_controller.user_delete);
 
 // user clear notifications
-router.post('/clearnotifications', user_controller.user_clear_notifications);
+router.post("/clearnotifications", user_controller.user_clear_notifications);
 
 module.exports = router;
